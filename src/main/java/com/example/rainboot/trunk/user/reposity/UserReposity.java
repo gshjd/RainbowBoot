b887bb2a3e9c8df70aac0fb015420d2e51c6e94a
lijunhao <lijunhao@worken.cn>
2019-12-26 10:40:06 +0800

    增加rsa工具

--- src/main/java/com/example/rainboot/trunk/user/reposity/UserReposity.java
+++
@@ -1,8 +0,0 @@
-package com.example.rainboot.trunk.user.reposity;
-
-import com.example.rainboot.trunk.user.model.UserUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserReposity extends JpaRepository<UserUser, Long> {
-}
